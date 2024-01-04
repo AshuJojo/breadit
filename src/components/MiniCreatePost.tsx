ab77262cc3231b0c605d86a2d72edb83573e6b9e
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-01-04 12:59:02 +0530

    Minor bug fixes

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -16,7 +16,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
     const router = useRouter();
     const pathName = usePathname();
 
-    return <li className='overflow-hidden rounded-md bg-white shadow'>
+    return <li className='overflow-hidden rounded-md bg-white shadow list-none'>
         <div className='h-full px-6 py-4 flex justify-between gap-6'>
             <div className='relative'>
                 <UserAvatar
